079fc9d0c6de74f4817adebfac46b7fdfd6b76aa
Mateusz Rosiński <matirosinski02@gmail.com>
2022-06-21 12:11:37 +0200

    Added new functionality buttons

--- Components/Buttons.js
+++ Components/Buttons.js
@@ -1,32 +1,41 @@
 import { UI } from "./UI.js";
 
 export class Buttons extends UI {
-  constructor(hit, stand) {
+  constructor(hit, double,stand, insurance) {
     super();
     this.initButtons();
     this.hitAction = hit;
+    this.doubleAction = double;
     this.standAction = stand;
+    this.insuranceAction = insurance;
     this.addListenersOnButton();
   }
 
   initButtons = () => {
     this.hitButton = this.createButton('HIT', 'hit');
-    this.getElement(this.selectors.buttons).appendChild(this.hitButton);
+    this.getElement(this.selectors.hitContener).appendChild(this.hitButton);
     
     this.standButton = this.createButton('STAND', 'stand');
-    this.getElement(this.selectors.buttons).appendChild(this.standButton);
+    this.getElement(this.selectors.standContener).appendChild(this.standButton);
 
     this.doubleButton = this.createButton('DOUBLE', 'double');
+    this.getElement(this.selectors.extendContener).appendChild(this.doubleButton);
+
     this.splitButton = this.createButton('SPLIT', 'split');
+
+    this.insuranceButton = this.createButton('INSURANCE', 'insurance');
   }
 
   addListenersOnButton = () => {
     this.hitButton.addEventListener('click', this.hitAction);
     this.standButton.addEventListener('click', this.standAction);
+    this.doubleButton.addEventListener('click', this.doubleAction);
+    this.insuranceButton.addEventListener('click', this.insuranceAction)
   }
 
   removeListeners = () => {
     this.hitButton.removeEventListener('click', this.hitAction);
     this.standButton.removeEventListener('click', this.standAction);
+    this.doubleButton.removeEventListener('click', this.doubleAction);
   }
 }
