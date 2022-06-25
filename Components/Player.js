f1ce869834b15e5190a0670e03717303cfe1bc58
Mateusz Rosiński <matirosinski02@gmail.com>
2022-06-25 19:35:19 +0200

    Added function which create new box and manages it

--- Components/Player.js
+++ Components/Player.js
@@ -7,14 +7,24 @@ class Player extends Wallet {
     this.playerCash = this.cash;
     this.currentCards = [];
     this.playerPoints = null;
+    this.boxOnePoints = null;
+    this.boxTwoPoints = null;
 
     this.getBet = () => this.playerBet; //Function getting player bet
-    this.takeCard = card => this.currentCards.push(card); //Function putting new card to the player deck
+    this.takeCard = (card, number) => { //Function putting new card to the player deck
+      if(!number) {
+        this.currentCards.push(card);
+      } else if(number === 1) {
+        this.boxOne.push(card);
+      } else if(number === 2) {
+        this.boxTwo.push(card);
+      }
+    } 
   }
 
   //Function which counts player points
-  playerPointsOperation = () => {
-    const cards = this.currentCards;
+  playerPointsOperation = (splitOperation = 0) => {
+    const cards = splitOperation ? (splitOperation === 1 ? this.boxOne : this.boxTwo) : this.currentCards;
     let amountAll = null;
 
     cards.forEach((card) => {
@@ -32,9 +42,36 @@ class Player extends Wallet {
         amountAll += card.valueCard;
       })
     }
+    
+    if(!splitOperation) {
+      this.playerPoints = amountAll;
+    } else if(splitOperation === 1) {
+      this.boxOnePoints = amountAll;
+    } else if(splitOperation === 2) {
+      this.boxTwoPoints = amountAll;
+    }
 
-    this.playerPoints = amountAll;
     return amountAll;
+  }
+
+  createBoxesElement = (boxOne, boxTwo) => {
+    this.playerPoints = null;
+
+    this.boxOne = [];
+    this.boxTwo = [];
+
+    for(let i = 0; i <= this.currentCards.length; i++) {
+      const existCard = this.currentCards.shift();
+
+      if(i === 0) {
+        this.boxOne.push(existCard);
+        boxOne.appendChild(this.boxOne[0].renderCard());
+
+      } else if(i === 1) {
+        this.boxTwo.push(existCard);
+        boxTwo.appendChild(this.boxTwo[0].renderCard());
+      }
+    }
   }
   
   get getPlayerCash() {
