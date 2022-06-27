7f05f8cf6d9cbc4a7a7a1008bd27eaf623ea0b28
Mateusz Rosiński <matirosinski02@gmail.com>
2022-06-27 20:48:43 +0200

    Solved wallet class which answering for cash in DB

--- Components/Wallet.js
+++ Components/Wallet.js
@@ -1,5 +1,37 @@
 export class Wallet {
-  constructor(cash) {
-    this.cash = cash
+  constructor() {
+    this.cash = null;
+    this.getMoneyFromDB()
+  }
+
+  getMoneyFromDB = async () => {
+    new Promise((resolve, reject) => {
+      resolve(fetch('http://localhost:3000/money'));
+      
+    }).then(res => {
+      const data = res.json();
+      return data;
+
+    }).then(res => {
+      const cash = res[0].cash;
+      return cash;
+      
+    }).then( res => {
+      this.cash = res
+    })
+  }
+
+  sendMoneyToDB = async (cashToSend) => {
+    const item = {
+      cash: cashToSend
+    }
+
+    await fetch('http://localhost:3000/money/cash', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(item)
+    })
   }
 }
